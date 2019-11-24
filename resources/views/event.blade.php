97c6bfd32f4f7f2bd7e61da175975b55240f7f3b
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2019-11-24 16:05:04 +0200

    Added the countdown timer

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -4,11 +4,19 @@
 @section('content')
 
 <section class="event-section">
-    <ul class="timer">
-        <li> <span class="name">{{ __("app.day") }}</span><span class="time">11:</span></li>
-        <li> <span class="name">{{ __("app.hour") }}</span><span class="time">20:</span></li>
-        <li> <span class="name">{{ __("app.minute") }}</span><span class="time">30:</span></li>
-        <li> <span class="name">{{ __("app.second") }}</span><span class="time">32</span></li>
+    <ul class="timer" id="timer" data-time="{{ strtotime($event->date) }}">
+        <li> 
+            <span class="name">{{ __("app.day") }}</span>
+            <span class="time" id="day">00:</span></li>
+        <li> 
+            <span class="name">{{ __("app.hour") }}</span>
+            <span class="time" id="hour">00:</span></li>
+        <li> 
+            <span class="name">{{ __("app.minute") }}</span>
+            <span class="time" id="min">00:</span></li>
+        <li> 
+            <span class="name">{{ __("app.second") }}</span>
+            <span class="time" id="sec">00</span></li>
     </ul>
     <div class="event-map" id="map"> </div>
     <div class="container">
@@ -86,6 +94,57 @@
 {{-- Script for the map & comment form --}}
 @section('script')
 <script>
+    // =================
+    // Event count down
+    // =================
+
+    // Adds zero to the number if it's less than 10
+    function addZero(num){
+        num = parseInt(num);
+        return num < 10 ? '0' + num : num;
+    }
+
+    let timer = document.getElementById("timer");
+    let dayElem = $("#day");
+    let hourElem = $("#hour");
+    let minElem = $("#min");
+    let secElem = $("#sec");
+
+    let theDate = new Date(parseInt(timer.dataset.time) * 1000);
+
+
+
+    let interval = setInterval(function(){
+        let now = new Date();
+
+        let diff = theDate.getTime() - now.getTime();
+
+        if(diff <= 0){
+            clearInterval(interval);
+            $("#timer").addClass('done');
+            return;
+        }
+
+        let sec = Math.floor(diff / 1000);
+        let min = Math.floor(sec / 60);
+        let hour = Math.floor(min / 60);
+        let day = Math.floor(hour / 24);
+        
+        hour %= 24;
+        min %= 60;
+        sec %= 60;
+
+
+        dayElem.text(addZero(day) + ":");
+        hourElem.text(addZero(hour) + ":");
+        minElem.text(addZero(min) + ":");
+        secElem.text(addZero(sec));
+
+    }, 1000)
+
+    // =================
+    //  Google map
+    // =================
     function initMap(){
             let position = {lat: {{ $event->lat }}, lng: {{ $event->lng }} };
             let map = new google.maps.Map(document.getElementById('map'),{
